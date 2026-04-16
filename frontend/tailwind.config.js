fca569d15dbc2d7d529eec1450c2a2038c6ebef9
Piyush Jha <piyushjha1625@gmail.com>
2026-04-16 10:09:54 +0530

    style: Add Tailwind CSS configuration with custom theme

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          900: "#0f172a",
+          800: "#1e293b",
+          700: "#334155",
+        },
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
